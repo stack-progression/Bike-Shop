102755b7430afd1c6e95283287c7b8d86f01266c
Andrei <stack.progression@gmail.com>
2026-02-07 22:37:51 +0200

    data base config

--- DataBase/index.js
+++ DataBase/index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json'); // se uită la db.json din acest folder
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
