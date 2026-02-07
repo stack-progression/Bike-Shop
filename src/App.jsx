025d1ed15a05d622800fba925710d88c4158db75
Andrei <stack.progression@gmail.com>
2026-02-07 22:29:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+          <img src="https://bike-images.andrew-code.com/Bike%20Image/Electric%20Bikes/turbo-vado-sl-40-step-through-eq/photos2/p1.webp" className="logo react" alt="React logo" />
+    </>
+  )
+}
+
+export default App
